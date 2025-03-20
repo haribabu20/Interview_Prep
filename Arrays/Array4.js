047cdd426a178b8169d7ba11127db89bd092383f
haribabu20 <166986653+haribabu20@users.noreply.github.com>
2025-03-20 10:53:49 +0530

    Array 4 - max count of 1's in array

--- Arrays/Array4.js
+++ Arrays/Array4.js
@@ -17,3 +17,35 @@ const maximumCount1 = (arr) => {
 
 
 console.log(maximumCount1([1,1,0,0,1,0,1,1,1,0,1,1,1,1,1]));
+
+
+/*
+
+line 9 can also be written as
+if(count>maxCount){
+maxCount=count;
+}
+
+
+
+Explanation:
+    I wrote the code like this --> maxCount = count;
+If it is in this approach then, this means: "Every time I find a 1, I set maxCount to the current count."
+Prblm:
+Let’s say count = 2, then later count = 1, you'll still assign maxCount = 1, even though the previous streak was longer! So maxCount will not store the true maximum streak of 1s.
+
+Example:
+  arr = [1, 1, 0, 1]
+  Your original code:
+  count = 2, maxCount = 2
+  Reset count = 0
+  count = 1, and now maxCount = 1 (overwrites 2 😱)
+  Result = 1 ❌ (Incorrect)
+
+---->  You should only update maxCount if the current count exceeds the previous maxCount. 
+
+
+
+
+
+*/

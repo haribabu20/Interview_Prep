5e71fe7d201bbec92d3c45ebbe37f40888454785
haribabu20 <166986653+haribabu20@users.noreply.github.com>
2025-03-20 10:18:55 +0530

    Array1 - 2nd largest element in an array.

--- Arrays/Array1.js
+++ Arrays/Array1.js
@@ -21,6 +21,8 @@ console.log(largestElement([1,3,5,7,3,2,9,11]));
 
 
 
+
+
 /*
 1) in If condition, we already knew that we are going to assign value to first. Hence we are moving initially the value of first to second.
 2) we have to assgin the looped value to second only when it is grater that exsiting second value and should not equal to first value

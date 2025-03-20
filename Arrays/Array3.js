06887bddb203ba1a7b167fa21b82c036a364b225
haribabu20 <166986653+haribabu20@users.noreply.github.com>
2025-03-20 10:14:43 +0530

    Update Array3.js

--- Arrays/Array3.js
+++ Arrays/Array3.js
@@ -33,3 +33,36 @@ const mergeArray = (arr1, arr2) => {
 
 console.log(mergeArray([1,3,5,7], [2,6,9]));
 
+
+
+/*
+arr1 = [1, 4, 7, 9]
+arr2 = [2, 3, 5, 6, 8, 10]
+
+The main loop runs until one of the arrays is exhausted. arr1[i] is undefined (because i=4 and arr1 has only 4 elements: index 0 to 3).
+So arr1 is done, but arr2 still has 8, 10 left.
+
+Without the extra loops:
+---> Those 8, 10 are lost, never added. <----
+
+hence we have that remaining element from arr1 and arr2
+
+
+Mistakes:
+1) while adding the remaining array elements, i used arr1[i].length instead of arr1.length
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+

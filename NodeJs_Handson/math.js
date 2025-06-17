58e11c08fdd6fef1135c89b5d84bcac8b2c7b7dc
haribabu20 <hariselva2000@gmail.com>
2025-06-17 14:19:19 +0530

    Node JS tutorial 30 min

---
+++ NodeJs_Handson/math.js
@@ -0,0 +1,15 @@
+
+// method 1 
+const add = (a,b) => a+b;
+const sub = (a,b) => a-b;
+const mul = (a,b) => a*b;
+const div = (a,b) => a/b;
+module.exports =  {add, sub, mul, div};
+
+
+
+// method 2
+exports.add1 = (a,b) => a+b;
+exports.sub1 = (a,b) => a-b;
+exports.mul1 = (a,b) => a*b;
+exports.div1 = (a,b) => a/b;
---
+++ NodeJs_Handson/server.js
@@ -0,0 +1,7 @@
+
+const {add, sub, mul, div} = require('./math')
+
+console.log(add(4,2));
+console.log(sub(4,2));
+console.log(mul(4,2));
+console.log(div(4,2));

3368cf705cbe6e2434255afeb5eaee4c68d1b973
haribabu20 <hariselva2000@gmail.com>
2025-06-17 22:44:46 +0530

    NPM Explanation

---
+++ NodeJs_Handson/stream.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+const rs = fs.createReadStream(path.join(__dirname,'files','bigfile.txt'),{encoding:'utf8'})
+const ws = fs.createWriteStream(path.join(__dirname,'files','new_bigfile.txt'))
+
+rs.on('data',(dataChunk)=>{
+  ws.write(dataChunk);
+})
+
+// instead of the above 3 line of code, we can write this one line
+rs.pipe(ws);

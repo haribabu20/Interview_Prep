cf0db9e252696268888ed7b2c487e4571277d0b4
haribabu20 <hariselva2000@gmail.com>
2025-06-19 23:49:11 +0530

    project related question 1

---
+++ Mock_Interview/Company /TCMock.js
@@ -0,0 +1,45 @@
+
+/*
+
+
+1. Material UI --> Y we have floating button
+
+2. Cloud knowledge -> 
+
+3. deployment involvement !
+
+
+
+
+
+Handson:
+
+1. button --> 100th time click panathum disable agirum.
+
+2. redux, api handling, error logging, 
+
+
+if api is not provided, then how you'll process. ---> online symulator la sample json i'll generate
+
+- merge conflict. --> cheery pick
+
+
+lead kitta reqd varum
+
+we'll discuss
+
+frontend la enalaam api theva paadum nu papom frintend team
+
+if puthu api venuna, sample json kepom from backend team. with that we'll do frontend work.
+
+
+y react and not angular
+
+api code
+
+
+vi 
+
+
+
+*/
